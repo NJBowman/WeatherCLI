7585a69a8f71a4b8c749456864cfa75c98b9608a
Nick Bowman <42209122+NJBowman@users.noreply.github.com>
2020-04-13 11:46:38 +0100

    Console app initialized

---
+++ WeatherCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
